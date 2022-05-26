27915fe61c160fb4cb8cee5cf5c935fc89ea813c
rivka123 <gasasa255@gmail.com>
2022-05-26 18:38:35 +0300

    update!

--- server/servertest.py
+++ server/servertest.py
@@ -1,14 +1,16 @@
 import requests
+import pytest
 
 class TestSearch():
 
+    @pytest.mark.sanity
     def test_search_valid_product(self):
         url = 'https://ivolunteer-app.herokuapp.com/users/login'
         myobj = {"Email": "Gasasa255@gmail.com","Password": "123456"}
         x = requests.post(url, data=myobj)
         assert x.status_code == 500
         assert x.elapsed.total_seconds() < 20
-
+    @pytest.mark.sanity
     def test_register(self):
         url = 'https://ivolunteer-app.herokuapp.com/users/register'
         myobj = {"FirstName": "rivka","LastName": "gasasa","Email": "Gasasa255@gmail.com","Password": "123456",
