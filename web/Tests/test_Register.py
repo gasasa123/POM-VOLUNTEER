a0cadfb5a04fc2cf6e369eed62017f799b2ac046
rivka123 <gasasa255@gmail.com>
2022-06-21 13:34:21 +0300

    update!

--- web/Tests/test_Register.py
+++ web/Tests/test_Register.py
@@ -1,4 +1,3 @@
-import allure
 from web.Base.base import Base
 from web.Page.Registerpage import RegisterPage
 import pytest
@@ -6,7 +5,6 @@ from web.Locator.RegisterLocator import Registerlocator
 
 @pytest.mark.usefixtures('set_up')
 class TestLogin(Base):
-
 
     def test_register_success(self):
         driver = self.driver
